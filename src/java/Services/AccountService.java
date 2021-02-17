3a80c253ce28e20de91e332049c2ab17e055c7cc
815267 <815267@SID0815267w2.ACDM.DS.SAIT.CA>
2021-02-16 17:13:15 -0700

    Fixed problem with adam account being acsessed with any password

--- src/java/Services/AccountService.java
+++ src/java/Services/AccountService.java
@@ -5,7 +5,7 @@ public class AccountService {
 
     public User login(String username, String password){
         //if the username = adam or betty and the password = password
-        if(username.equals("adam") || username.equals("betty") && (password.equals("password"))){
+        if(username.equals("adam") && (password.equals("password")) || username.equals("betty") && (password.equals("password"))){
             //return the username and make the password null
             return new User(username,null);
         }
